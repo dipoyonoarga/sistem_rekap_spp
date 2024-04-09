4cd6216cf45547c387b54ac547d57e3f5b912457
Arga <dipoyonoarga@gmail.com>
2024-04-09 10:16:34 +0700

    fix

--- admin/tambah-siswa.php
+++ admin/tambah-siswa.php
@@ -8,11 +8,11 @@
     </div>
     <div class="form-groub mb-2">
         <label>NIS</label>
-        <input type="number" name="nis"  class="form-control" required>
+        <input type="number" name="nis" class="form-control" required>
     </div>
     <div class="form-groub mb-2">
         <label>Nama</label>
-        <input type="text" name="nama"  class="form-control" required>
+        <input type="text" name="nama" class="form-control" required>
     </div>
     <!-- banyak relasi -->
     <div class="form-groub mb-2">
@@ -20,15 +20,13 @@
         <select name="id_kelas" class="form-control" required>
             <option value="">Pilih Kelas</option>
             <!-- ambil database kelas -->
-            <? php 
+            <?php
             // koneksi
-            include'../koneksi.php';
+            include '../koneksi.php';
             $kelas = mysqli_query($koneksi, "SELECT*FROM kelas ORDER BY nama_kelas ASC");
             foreach ($kelas as $data_kelas) {
             ?>
-            <option value="<?=$data_kelas['id_kelas']?>" <?=$data_kelas['nama_kelas'];?>></option>
-   
-            
+                <option value="<?= $data_kelas['id_kelas'] ?>"><?= $data_kelas['nama_kelas']; ?></option>
             <?php } ?>
         </select>
     </div>
