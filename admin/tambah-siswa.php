fd148cef51e20e7c698c8535460c0fa4b4e6146c
Arga <dipoyonoarga@gmail.com>
2024-04-10 20:01:37 +0700

    tambah-siswa done

--- admin/tambah-siswa.php
+++ admin/tambah-siswa.php
@@ -26,10 +26,35 @@
             $kelas = mysqli_query($koneksi, "SELECT*FROM kelas ORDER BY nama_kelas ASC");
             foreach ($kelas as $data_kelas) {
             ?>
+                <!-- untuk memunculkan opsi kelas yang sudah di add-->
                 <option value="<?= $data_kelas['id_kelas'] ?>"><?= $data_kelas['nama_kelas']; ?></option>
-            <?php } ?>
+            <?php  } ?>
         </select>
     </div>
+    <!-- alamat menggunakan text area -->
+    <div class="form-groub mb-2">
+        <label>Alamat</label>
+        <textarea name="alamat" class="form-control" required></textarea>
+    </div>
+
+    <div class="form-groub mb-2">
+        <label>SPP</label>
+        <select name="id_spp" class="form-control" required>
+            <option value="">Pilih SPP</option>
+            <!-- ambil database kelas -->
+            <?php
+            // koneksi
+            include '../koneksi.php';
+            $spp = mysqli_query($koneksi, "SELECT*FROM spp ORDER BY id_spp ASC");
+            foreach ($spp as $data_spp) {
+            ?>
+                <!-- untuk memunculkan opsi kelas yang sudah di add-->
+                <option value="<?= $data_spp['id_spp'] ?>"><?= $data_spp['tahun']; ?> | <?= number_format($data_spp['nominal'], 2, ',', '.');
+                                                                                        $data_spp['nominal']; ?></option>
+            <?php  } ?>
+        </select>
+    </div>
+
     <div class="form-group">
         <button type="submit" class="btn btn-success">SIMPAN</button>
         <button type="reset" class="btn btn-warning">KOSONGKAN</button>
